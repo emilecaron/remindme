7606d0cd780574713dcbc16beff3d76911bbb72e
Emile CARON <ecaron@work4labs.com>
2014-11-28 22:49:06 +0100

    Backboning on going

--- app/static/script.js
+++ app/static/script.js
@@ -1,25 +1,123 @@
-var on_submit=function() {
-    console.log('loading');
 
-    var form = {
-        "email": $('#email').val(),
+$(function(){
+  console.log('Backbone time');
+
+  var Message = Backbone.Model.extend({
+
+    defaults: function() {
+      return {
+        title: 'TITLE',
+        msg: 'MSG',
+        hidden: true,
+        type: "warning"
+      };
+    }
+
+  });
+  
+
+  var MsgView = Backbone.View.extend({
+
+    el:  $("#msg-div"),
+
+    // Cache the template function
+    template: _.template($('#msg-template').html()),
+
+    // The DOM events specific to an item.
+    events: {
+      "click .close"      : "close"
+    },
+
+    initialize: function() {
+      this.listenTo(this.model, 'destroy', this.remove);
+    },
+
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+    // Remove the msg, destroy the model.
+    close: function() {
+      this.model.destroy();
+    }
+
+  });
+
+  var FormView = Backbone.View.extend({
+    
+    el: $('#form-div'),
+
+    template: _.template($('#form-template').html()),
+
+    events: {
+      "keypress #email":  "submitOnEnter"
+    },
+
+    render: function() {
+      this.$el.html(this.template());
+      return this;
+    },
+
+    submitOnEnter: function() {
+      if (e.keyCode != 13) return;
+      if (!this.input.val()) return;
+      // send the shit
+
+      console.log('loading');
+
+      var form = {
+        "email": $('#email').val(),  // Must do something for that
         "date": $('#date').val()
-    };
-    $.post('http://localhost:5000/api/register', form, on_reply, 'json').always(function() {
+      };
+      $.post('http://localhost:5000/api/register', form, this.onReply, 'json').always(function() {
         console.log('loaded');
         $('#alert').removeClass('hidden');
-    });
-};
+      });
+    },
 
-var on_reply=function(data){
-    console.log('reply');
-    console.log(data);
-    $('#alert-msg').html(data.msg);
-}
+    onReply: function(data) {
+        console.log('reply');
+        console.log(data);
+        //$('#alert-msg').html(data.msg);
+        var view = new MsgView({title: 'Yeah', msg:data.msg});
+        this.$("#msg-div").append(view.render().el);
+    }
+
+  });
+
+  var AppView = Backbone.View.extend({
+
+    el: $("#site-wrapper"),
+
+    formTemplate: _.template($('#form-template').html()),
+
+
+
+    initialize: function() {
+        var form = new FormView();
+
+    },
+
+    // Re-rendering the App just means refreshing the statistics -- the rest
+    // of the app doesn't change.
+    render: function() {
+        //
+    }
+  });
+
+
+  var App = new AppView;
+
+});
+
+
+
 
 
 
 $(document).ready(function() {
+    return;
     console.log('ready');
 
     // bind
